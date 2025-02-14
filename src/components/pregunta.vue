94d6a473284c166a33ed3557dc2abd839eaf698c
Ignacioorosco <nachoron32@gmail.com>
2025-02-14 17:26:02 -0300

    subir actualizado

--- src/components/pregunta.vue
+++ src/components/pregunta.vue
@@ -8,7 +8,7 @@
     
     <img src="/src/assets/heart-png.webp" alt="Foto" class="imagen scale-up-center">
     <img src="/src/assets/heart-png.webp" alt="Foto" class="imagen2">
-    <img src="/src/assets/heart-png.webp" alt="Foto" class="imagen3">
+
     <br>
     
     
