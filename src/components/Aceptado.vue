186c824bc19f182d9c3b0bbc6de5c203505f9d42
Ignacioorosco <nachoron32@gmail.com>
2025-02-14 17:33:06 -0300

    subir actualizado 3

--- src/components/Aceptado.vue
+++ src/components/Aceptado.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="container">
-      <h1>Era obvio que ibas a decir que sí </h1>
+      <h1>Era obvio que ibas a decir que sí ❣️ </h1>
       <img src="/src/assets/b1325f3584fc7b4443526798cee3015d.gif" alt="GIF animado" class="gif">
          <h2>te mereces un regalo amor</h2>
-         <footer>Tu novio el Nachito que te ama</footer>
+         <footer>Tu novio el Nachito que te ama ❣️</footer>
     </div>
   
   </template>
